b44f0912eb8c23476fed8c1ef16dbc8676837ac5
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-09-21 10:08:14 -0600

    Added required files, but no query/mutation definitions yet

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category, Order } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {},
+    Mutation: {}
+};
+
+module.exports = resolvers;
