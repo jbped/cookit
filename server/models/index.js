1648a3c4769ec94480adec8c7f9c86819ec0954f
BoDee <nyx0hemir@gmail.com>
2021-09-24 19:22:33 -0600

    finished deleteRecipe resolver

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Ingredient = require('./Ingredient');
 const Recipe = require('./Recipe');
-const Step = require('./Step')
+const Step = require('./Step');
+const Cookware = require('./Cookware');
 
 module.exports = { User, Ingredient, Recipe, Step };
