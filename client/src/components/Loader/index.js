9fa11520558eea4692ff3bc21b3b8a4e384e7ebc
Jake P <76881086+jbped@users.noreply.github.com>
2021-09-28 22:05:53 -0600

    add loader component

---
+++ client/src/components/Loader/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Box from '@mui/material/Box';
+import { CircularProgress, Typography } from '@mui/material';
+
+export default function Loader() {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Typography variant="h2" component="div" color="primary" fontWeight="bold" sx={{ flexGrow: 1, textAlign: 'center', m: 0 }}>
+        Coo<Typography component="span" variant="h2" color="secondary" fontStyle="italic" fontWeight="bold" sx={{ flexGrow: 1 }}>Kit</Typography>
+      </Typography>
+      <CircularProgress
+        variant="indeterminate"
+        disableShrink
+        color="secondary"
+        sx={{
+          animationDuration: '550ms',
+        }}
+        size={75}
+        thickness={6}
+      />
+    </Box>
+  )
+} 
