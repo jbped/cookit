121166741a716cd8081833ab7f4048cfea2af7c0
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 22:22:19 -0600

    server edit... again

--- server/server.js
+++ server/server.js
@@ -34,12 +34,15 @@ app.use(express.json());
 // });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
 
 db.once('open', () => {
     app.listen(PORT, () => {
