c5d7ff34d14e638c6f94d68745faf8732356a466
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 22:23:00 -0600

    server edit... againx2

--- server/server.js
+++ server/server.js
@@ -38,7 +38,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
