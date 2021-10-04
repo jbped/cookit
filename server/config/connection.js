0f47cb1700609ae16e872e8669b38d868095cf12
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 21:15:53 -0600

    config edits

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cookit', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
