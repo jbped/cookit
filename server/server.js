b02807938d5c5c7e752e85c847f8a2c1063eccca
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 22:17:58 -0600

    server edit

--- server/server.js
+++ server/server.js
@@ -25,13 +25,21 @@ app.use(express.json());
 
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
