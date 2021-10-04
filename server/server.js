653e8326a555d67570d602dded1ffe4eb9b59c87
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 23:22:44 -0600

    new package.json script

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ app.use(express.json());
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));  
+  app.use(express.static(path.resolve(__dirname, '../client/build')));  
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
 // if (process.env.NODE_ENV === 'production') {
