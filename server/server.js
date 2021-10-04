4329cb495f49b3b9bc54c00c7244f89290b204a4
Jake P <76881086+jbped@users.noreply.github.com>
2021-10-03 22:31:43 -0600

    how many times are we gonna do this cli? I feel really aquainted with you tonight...

--- server/server.js
+++ server/server.js
@@ -25,23 +25,24 @@ app.use(express.json());
 
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
-
 if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('../client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   // Exprees will serve up production assets
+//   app.use(express.static('../client/build'));
+
+//   // Express serve up index.html file if it doesn't recognize route
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//   });
+// }
 
 db.once('open', () => {
     app.listen(PORT, () => {
